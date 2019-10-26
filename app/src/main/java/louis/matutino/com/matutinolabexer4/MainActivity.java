8f5185122dbd3e20a15f5d5afac77b70a7eb7141
Louis <2015081544@ust-ics.mygbiz.com>
2019-10-26 15:27:56 +0800

    Initial commit

---
+++ app/src/main/java/louis/matutino/com/matutinolabexer4/MainActivity.java
@@ -0,0 +1,65 @@
+package louis.matutino.com.matutinolabexer4;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    String[] verNames, country, industry, ceo, info;
+    int[] logo = {R.drawable.icbc
+            ,R.drawable.jpmorgan,R.drawable.chinaconstruction,
+            R.drawable.agricultural,R.drawable.america,R.drawable.apple,R.drawable.pingan,R.drawable.bankofchina,
+            R.drawable.royaldutch,R.drawable.wellsfargo,R.drawable.exxonmobil,R.drawable.att,R.drawable.samsung,R.drawable.citigroup};
+    ArrayList<AndroidVersion> versions=new ArrayList<>();
+
+    ListView lstVersions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        verNames = getResources().getStringArray(R.array.VerName);
+        country = getResources().getStringArray(R.array.varCountry);
+        industry = getResources().getStringArray(R.array.varIndustry);
+        ceo = getResources().getStringArray(R.array.varCEO);
+        info = getResources().getStringArray(R.array.varInfo);
+
+        for(int i = 0; i < verNames.length; i++){
+            versions.add(new AndroidVersion(logo[i], verNames[i], country[i], industry[i], ceo[i]));
+
+        }
+
+
+        AndroidAdapter adapter = new AndroidAdapter(this, R.layout.row, versions);
+        lstVersions = findViewById(R.id.lvAndroid);
+        lstVersions.setAdapter(adapter);
+        lstVersions.setOnItemClickListener(this);
+
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, final int i, long id) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setIcon(logo[i]);
+        dialog.setTitle(verNames[i]);
+        dialog.setMessage(info[i]);
+        dialog.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, verNames[i], Toast.LENGTH_LONG).show();
+            }
+        });
+        dialog.create().show();
+    }
+}
